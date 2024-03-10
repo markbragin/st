e5fdaedcad31d5f9ed8ffb0ece61451ba23e334c
mark bragin <m4rk.brag1n@gmail.com>
2024-03-10 17:12:17 +0300

    changed font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMonoNerdFont-Medium:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrainsMono-Bold:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 unsigned int selectionbg = 0;
